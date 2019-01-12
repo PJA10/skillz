63e28aa8a689bb1cb91a16f021f10f9430768dbb
PJA10 <alon.w.10@gmail.com>
2019-01-12 02:40:27 +0200

    update

--- Defense.py
+++ Defense.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 
 from elf_kingdom import *
 from trainingBot import *
 from MyBot import *
 from Slider import *
 
-def Enemy_elf_nearby(game,portal):
+
+def enemy_elf_nearby(game,portal):
     """
 
     This function checks if one of the enemy elves threatening the given portal. It checks if one of the enemy elves can
@@ -15,5 +15,5 @@ def Enemy_elf_nearby(game,portal):
     :return: if one of the enemy elves is a threat to the given portal
     :type: bool
     """
-        if portal.get_location().distance(get_closest_enemy_elf().get_location()) <= : #if the closest enemy elf can get to the given po
+        pass #if the closest enemy elf can get to the given po
 
